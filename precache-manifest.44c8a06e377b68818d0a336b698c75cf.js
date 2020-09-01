feb6f7314fba2156a10c7b9ed26b8e7c28156bb1
andrewroar <59775822+andrewroar@users.noreply.github.com>
2020-09-01 18:46:24 +0100

    Updates

---
+++ precache-manifest.44c8a06e377b68818d0a336b698c75cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52e6a71f5285659ffc5a95294bcb3986",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "fa6a4d760ae5b6f7148a",
+    "url": "/react-portfolio/static/css/main.c9a27abe.chunk.css"
+  },
+  {
+    "revision": "a671751e56d62c9f6a58",
+    "url": "/react-portfolio/static/js/2.c8868c4b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.c8868c4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa6a4d760ae5b6f7148a",
+    "url": "/react-portfolio/static/js/main.e666246f.chunk.js"
+  },
+  {
+    "revision": "bc4f645bd9fafef47195",
+    "url": "/react-portfolio/static/js/runtime-main.fc68917a.js"
+  }
+]);
